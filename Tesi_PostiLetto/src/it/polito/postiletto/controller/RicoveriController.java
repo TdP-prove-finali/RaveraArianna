df83a6d7a302717f7e84781b036430c3ddba7cb5
Arianna <Arianna@LAPTOP-LPB65445>
2019-05-17 20:50:49 +0200

    Implementata la funzione di simulazione. Aggiunti i db nella cartella.

--- Tesi_PostiLetto/src/it/polito/postiletto/controller/RicoveriController.java
+++ Tesi_PostiLetto/src/it/polito/postiletto/controller/RicoveriController.java
@@ -11,14 +11,20 @@ import java.util.ResourceBundle;
 
 import it.polito.postiletto.model.Model;
 import it.polito.postiletto.model.Row;
+import it.polito.postiletto.model.Row2;
 import javafx.collections.*;
 import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
@@ -32,6 +38,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class RicoveriController {
@@ -79,6 +86,15 @@ public class RicoveriController {
 
     @FXML
     private TableColumn<Row, Double> colMSDP;
+    
+    @FXML
+    private LineChart<String, Double> grafo;
+
+    @FXML
+    private CategoryAxis giorniGrafo;
+
+    @FXML
+    private NumberAxis occupazioneGrafo;
 
     @FXML
     private PieChart torta;
@@ -93,6 +109,15 @@ public class RicoveriController {
     private DatePicker dateFineRicovero;
 
     @FXML
+    private Label txt1;
+
+    @FXML
+    private Label txt2;
+
+    @FXML
+    private ChoiceBox<Integer> giorniPost;
+
+    @FXML
     private ComboBox<String> cbRepartoRicovero;
     
     @FXML
@@ -105,19 +130,16 @@ public class RicoveriController {
     private Label labelInfo1;
 
     @FXML
-    private TableView<Row> tabellaS;
+    private TableView<Row2> tabellaS;
 
     @FXML
-    private TableColumn<Row, LocalDate> colDataS;
+    private TableColumn<Row2, LocalDate> colDataS;
 
     @FXML
-    private TableColumn<Row, Double> colPrevS;
+    private TableColumn<Row2, Double> colPrevS;
 
     @FXML
-    private TableColumn<Row, Integer> colOccS;
-
-    @FXML
-    private TableColumn<Row, Double> colMSDS;
+    private TableColumn<Row2, Integer> colOccS;
     
     @FXML
     private TextArea txtSimulazione;
@@ -132,6 +154,7 @@ public class RicoveriController {
     private Stage secondaryStage;
 
 
+    //FINITO
     @FXML
     void doCancella(ActionEvent event) {
     	cbRepartoPrevisione.setValue("");
@@ -141,37 +164,53 @@ public class RicoveriController {
     	dateFinePrevisione.setValue(null);
     	datiTorta = FXCollections.observableArrayList(new PieChart.Data("reparto", 100));
 		torta.setData(datiTorta);
+
+    	grafo.getData().clear();;
 		
 		ObservableList<Row> righe=FXCollections.observableArrayList();
 		tabellaP.setItems(righe);
+		ObservableList<Row2> righe2=FXCollections.observableArrayList();
+		tabellaS.setItems(righe2);
 		
 		labelInfo.setText("");
+		labelInfo1.setText("");
 		
     	txtSimulazione.clear();
     	
     	dateInizioRicovero.setValue(null);
     	dateFineRicovero.setValue(null);
+    	
+    	txt1.setVisible(false);
+		txt2.setVisible(false);
+		giorniPost.setVisible(false);
     }
 
-    //FINITO
+    // Rivedi grafo e problema torta che scompare
     @FXML
     void doPrevisione(ActionEvent event) {
     	LocalDate inizio=dateInizioPrevisione.getValue();
     	LocalDate fine=dateFinePrevisione.getValue();
     	String reparto=cbRepartoPrevisione.getValue();
     	double alfa=alpha.getValue();
+    	XYChart.Series<String, Double> series = new XYChart.Series<>();
+    	
+    	grafo.getData().clear();
 
 		model.creaPrevisioni(reparto, alfa);
     	
     	if(alfa==0.0||inizio==null||fine==null||reparto==null||reparto=="") {
     		labelInfo.setText("ATTENZIONE! Inserire tutti i dati.");
+    		labelInfo.setTextFill(Color.RED);
     	}
     	else if(fine.isBefore(inizio)) {
     		labelInfo.setText("ATTENZIONE! Inserire le date correttamente.");
+    		labelInfo.setTextFill(Color.RED);
     	}
     	else {
-    		
+    		labelInfo.setText("Posti letto disponibili nel reparto: "+model.occMax(reparto));
+    		labelInfo.setTextFill(Color.BLACK);
     		ObservableList<Row> righe=FXCollections.observableArrayList();
+    		tabellaP.setItems(righe);
     		for(Row r:model.previsione(inizio, fine, reparto)) {
     			righe.add(r);
     		}
@@ -184,8 +223,17 @@ public class RicoveriController {
     		tabellaP.setItems(righe);
     		
     		titoloTorta="Statistiche occupazione media del reparto di "+reparto.toLowerCase();
-    		datiTorta = FXCollections.observableArrayList(new PieChart.Data("Posti occupati", model.datiTortaPrev()), new PieChart.Data("Posti liberi", 100-(model.datiTortaPrev())));
+    		if(model.datiTortaPrev()>=100)
+    			datiTorta = FXCollections.observableArrayList(new PieChart.Data("Posti occupati", 100));
+    		else
+    			datiTorta = FXCollections.observableArrayList(new PieChart.Data("Posti occupati", model.datiTortaPrev()), new PieChart.Data("Posti liberi", 100-(model.datiTortaPrev())));
     		torta.setData(datiTorta);
+    		
+            //populating the series with data
+            for(Row r:model.previsione(inizio, fine, reparto)) {
+            	series.getData().add(new XYChart.Data<String, Double>(r.getData().getDayOfMonth()+"",r.getPrevisione()));
+            }
+            grafo.getData().add(series);
     	}
     }
 
@@ -194,36 +242,52 @@ public class RicoveriController {
     	LocalDate inizio=dateInizioRicovero.getValue();
     	LocalDate fine=dateFineRicovero.getValue();
     	String reparto=cbRepartoRicovero.getValue();
-    	double alfa=alpha.getValue();
+    	double alfa=alphaSimulazione.getValue();
+    	int post;
+    	if(giorniPost.isVisible())
+    		post=giorniPost.getValue();
 
 		model.creaPrevisioni(reparto, alfa);
+		model.creaPrevisioniDiGennaio(reparto, alfa);
     	
 		if(alfa==0.0||inizio==null||fine==null||reparto==null||reparto=="") {
     		labelInfo1.setText("ATTENZIONE! Inserire tutti i dati.");
+    		labelInfo1.setTextFill(Color.RED);
     	}
     	else if(fine.isBefore(inizio)) {
     		labelInfo1.setText("ATTENZIONE! Inserire le date correttamente.");
+    		labelInfo1.setTextFill(Color.RED);
     	}
     	else {
-    		
-    		ObservableList<Row> righe=FXCollections.observableArrayList();
-    		for(Row r:model.simulazione(inizio, fine, reparto)) {
+    		labelInfo1.setText("Posti letto disponibili nel reparto: "+model.occMax(reparto));
+    		labelInfo1.setTextFill(Color.BLACK);
+    		if(giorniPost!=null) {
+    			//calcola con posticipazione
+    		}
+    		ObservableList<Row2> righe=FXCollections.observableArrayList();
+    		for(Row2 r:model.simulazione(inizio, fine, reparto)) {
     			righe.add(r);
     		}
     		
-    		colDataS.setCellValueFactory(new PropertyValueFactory<Row, LocalDate>("data"));
-            colPrevS.setCellValueFactory(new PropertyValueFactory<Row, Double>("previsione"));
-            colOccS.setCellValueFactory(new PropertyValueFactory<Row, Integer>("domanda"));
-            colMSDS.setCellValueFactory(new PropertyValueFactory<Row, Double>("msd"));
+    		colDataS.setCellValueFactory(new PropertyValueFactory<Row2, LocalDate>("data"));
+            colPrevS.setCellValueFactory(new PropertyValueFactory<Row2, Double>("previsione"));
+            colOccS.setCellValueFactory(new PropertyValueFactory<Row2, Integer>("domanda"));
             
     		tabellaS.setItems(righe);
     		
     		String result=model.doIpotesi(model.simulazione(inizio, fine, reparto), reparto);
     		txtSimulazione.appendText(result);
+    		
+    		//se non c'è posto chiedo se posso posticipare
+    		if(model.posticipa()==true) {
+    			txt1.setVisible(true);
+    			txt2.setVisible(true);
+    			giorniPost.setVisible(true);
+    		}
     	}
     }
     
-    // FINITO
+    //FINITO
     @FXML
     void doEspandiGrafo(MouseEvent event) {
     	
@@ -250,35 +314,39 @@ public class RicoveriController {
 
     @FXML
     void initialize() {
-    	assert tabForecasting != null : "fx:id=\"tabForecasting\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert dateInizioPrevisione != null : "fx:id=\"dateInizioPrevisione\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert dateFinePrevisione != null : "fx:id=\"dateFinePrevisione\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert cbRepartoPrevisione != null : "fx:id=\"cbRepartoPrevisione\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert alpha != null : "fx:id=\"alpha\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert btnPrevisione != null : "fx:id=\"btnPrevisione\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert labelInfo != null : "fx:id=\"labelInfo\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert tabellaP != null : "fx:id=\"tabellaP\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert colDataP != null : "fx:id=\"colDataP\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert colPrevP != null : "fx:id=\"colPrevP\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert colOccP != null : "fx:id=\"colOccP\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert colMSDP != null : "fx:id=\"colMSDP\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert torta != null : "fx:id=\"torta\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert tabRicovero != null : "fx:id=\"tabRicovero\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert dateInizioRicovero != null : "fx:id=\"dateInizioRicovero\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert dateFineRicovero != null : "fx:id=\"dateFineRicovero\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert cbRepartoRicovero != null : "fx:id=\"cbRepartoRicovero\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert alphaSimulazione != null : "fx:id=\"alphaSimulazione\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert btnSimulazione != null : "fx:id=\"btnSimulazione\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert labelInfo1 != null : "fx:id=\"labelInfo1\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert tabellaS != null : "fx:id=\"tabellaS\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert colDataS != null : "fx:id=\"colDataS\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert colPrevS != null : "fx:id=\"colPrevS\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert colOccS != null : "fx:id=\"colOccS\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert colMSDS != null : "fx:id=\"colMSDS\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert txtSimulazione != null : "fx:id=\"txtSimulazione\" was not injected: check your FXML file 'PostiLetto.fxml'.";
-        assert btnCancella != null : "fx:id=\"btnCancella\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+    	 assert tabForecasting != null : "fx:id=\"tabForecasting\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert dateInizioPrevisione != null : "fx:id=\"dateInizioPrevisione\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert dateFinePrevisione != null : "fx:id=\"dateFinePrevisione\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert cbRepartoPrevisione != null : "fx:id=\"cbRepartoPrevisione\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert alpha != null : "fx:id=\"alpha\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert btnPrevisione != null : "fx:id=\"btnPrevisione\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert labelInfo != null : "fx:id=\"labelInfo\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert tabellaP != null : "fx:id=\"tabellaP\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert colDataP != null : "fx:id=\"colDataP\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert colPrevP != null : "fx:id=\"colPrevP\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert colOccP != null : "fx:id=\"colOccP\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert colMSDP != null : "fx:id=\"colMSDP\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert grafo != null : "fx:id=\"grafo\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert giorniGrafo != null : "fx:id=\"giorniGrafo\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert occupazioneGrafo != null : "fx:id=\"occupazioneGrafo\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert torta != null : "fx:id=\"torta\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert tabRicovero != null : "fx:id=\"tabRicovero\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert dateInizioRicovero != null : "fx:id=\"dateInizioRicovero\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert dateFineRicovero != null : "fx:id=\"dateFineRicovero\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert txt1 != null : "fx:id=\"txt1\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert txt2 != null : "fx:id=\"txt2\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert giorniPost != null : "fx:id=\"giorniPost\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert cbRepartoRicovero != null : "fx:id=\"cbRepartoRicovero\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert alphaSimulazione != null : "fx:id=\"alphaSimulazione\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert btnSimulazione != null : "fx:id=\"btnSimulazione\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert labelInfo1 != null : "fx:id=\"labelInfo1\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert tabellaS != null : "fx:id=\"tabellaS\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert colDataS != null : "fx:id=\"colDataS\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert colPrevS != null : "fx:id=\"colPrevS\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert colOccS != null : "fx:id=\"colOccS\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert txtSimulazione != null : "fx:id=\"txtSimulazione\" was not injected: check your FXML file 'PostiLetto.fxml'.";
+         assert btnCancella != null : "fx:id=\"btnCancella\" was not injected: check your FXML file 'PostiLetto.fxml'.";
 
-         
      	
         datiTorta = FXCollections.observableArrayList(new PieChart.Data("reparto", 100));
 		torta.setData(datiTorta);
--- Tesi_PostiLetto/src/it/polito/postiletto/model/Model.java
+++ Tesi_PostiLetto/src/it/polito/postiletto/model/Model.java
@@ -1,6 +1,5 @@
 package it.polito.postiletto.model;
 
-import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.util.*;
 
@@ -13,13 +12,17 @@ public class Model {
 	private Map<LocalDate, Double> previsioni;
 	private Map<LocalDate, Integer> domanda;
 	private Map<LocalDate, Double> msd;
+	private Map<LocalDate, Double> previsioniRicovero;
+	private Map<LocalDate, Integer> domandaRicovero;
+	private Map<LocalDate, Double> previsioniGennaio;
+	private boolean post;
 	
 	
 	public Model() {
 		datidao=new DatiDAO();
 	}
 	
-	//FINITO
+	//FINITO, lo richiamano subito entrambe le funzionalità per creare tutte le previsioni da giugno a dicembre
 	public void creaPrevisioni(String reparto, double alfa) {
 		msd=new HashMap<LocalDate, Double>();
 		previsioni=new HashMap<LocalDate, Double>();
@@ -47,6 +50,9 @@ public class Model {
 			//do
 			double f=alfa*domanda.get(inizio) + (1-alfa)*previsioni.get(inizio.minusWeeks(1));
 			previsioni.put(inizio, f);
+			if(inizio.getMonth().equals("JANUARY")) {
+				previsioniGennaio.put(inizio, f);
+			}
 			
 			//do
 			err+=Math.pow(previsioni.get(inizio)-domanda.get(inizio), 2);
@@ -59,7 +65,51 @@ public class Model {
 		}
 	}
 	
-	//FINITO
+	//FINITO, usato da creaPrevisione
+	public Map<LocalDate, Integer> occupazioneReparto(LocalDate in, LocalDate fin, String reparto) {
+			
+		Map<LocalDate, Integer> occ=new TreeMap<LocalDate, Integer>();
+			
+		while(in.isBefore(fin)||in.equals(fin)) {
+			//do
+			occ.put(in , datidao.occupazioneRepartoPrev(in, reparto));
+			//incremento
+			in=in.plusDays(1);
+		}
+		return occ;
+	}
+	
+	//FINITO, lo richiama la funzione di simulazione per creare la previsione dei gennaio 
+	public void creaPrevisioniDiGennaio(String reparto, Double alfa) {
+		previsioniRicovero=new HashMap<LocalDate, Double>();
+		domandaRicovero=this.occupazioneReparto(LocalDate.of(2018, 12, 1), LocalDate.of(2019, 01, 31), reparto);
+		
+		//per giugno F(t)=A(t)
+		LocalDate inizioDic=LocalDate.of(2018, 12, 1);
+		LocalDate fineDic=LocalDate.of(2018, 12, 31);
+		
+		while(inizioDic.isBefore(fineDic)||inizioDic.equals(fineDic)) {
+			//do
+			previsioniRicovero.put(inizioDic, (double)previsioni.get(inizioDic));
+			//incremento
+			inizioDic=inizioDic.plusDays(1);
+		}
+		
+		//da luglio a gennaio uso F(t-1) quindi quelle del mese precendente
+		LocalDate inizio=LocalDate.of(2019, 01, 1);
+		LocalDate fine=LocalDate.of(2019, 01, 31);
+		
+		//trovo alfa migliore
+		while(inizio.isBefore(fine)||inizio.equals(fine)) {
+			//do
+			double f=alfa*domandaRicovero.get(inizio) + (1-alfa)*previsioniRicovero.get(inizio.minusMonths(1));
+			previsioniRicovero.put(inizio, f);
+			//incremento
+			inizio=inizio.plusDays(1);
+		}	
+	}
+	
+	//FINITO, riempie la tabella della previsione
 	public List<Row> previsione(LocalDate inizio, LocalDate fine, String reparto) {
 		// stampare previsione e fare i due grafici
 	
@@ -85,65 +135,72 @@ public class Model {
 			//incremento
 			in=in.plusDays(1);
 		}
-		System.out.println(somma+" "+ngiorni+" "+nmax);
 		n=((float)((float)somma/ngiorni)/nmax)*100;
-		
 		return previsione;
 	}
 	
-	//FINITO
+	//FINITO, riempie la torta
 	public float datiTortaPrev() {
 		return n;
 	}
 
-	// FINITO
-	public Map<LocalDate, Integer> occupazioneReparto(LocalDate in, LocalDate fin, String reparto) {
-		
-		Map<LocalDate, Integer> occ=new TreeMap<LocalDate, Integer>();
-		
-		while(in.isBefore(fin)||in.equals(fin)) {
-			//do
-			occ.put(in , datidao.occupazioneRepartoPrev(in, reparto));
-			//incremento
-			in=in.plusDays(1);
-		}
-		return occ;
-	}
-	
-
-	public List<Row> simulazione(LocalDate inizio, LocalDate fine, String reparto) {
-		
-		// mi guardo anche i tot (2?) giorni prima e dopo per sapere se posso spostare il ricovero di qualche giorno
-		List<Row> previsione=new ArrayList<Row>();
-		int nmax=datidao.getNMaxPosti(reparto);
-		int somma=0;
-		int ngiorni=0;
+	//FINITO, riempie la tabella della simulazione
+	public List<Row2> simulazione(LocalDate inizio, LocalDate fine, String reparto) {
+		List<Row2> previsione=new ArrayList<Row2>();
 		LocalDate in=inizio; //piu o meno i giorni che sono disposto a posticipare o anticipare etc
 		
 		//do previsione
 		while(in.isBefore(fine)||in.equals(fine)) {
-			Row r= new Row(in,previsioni.get(in),domanda.get(in),msd.get(in));
+			Row2 r= new Row2(in,previsioniRicovero.get(in),domandaRicovero.get(in));
 			previsione.add(r);
 			in=in.plusDays(1);
 		}
-		
 		return previsione;
 	}
-
-	public String doIpotesi(List<Row> simulazione, String reparto) {
+	
+	// riempie la text area della simulazione
+	public String doIpotesi(List<Row2> simulazione, String reparto) {
 		Map<LocalDate, Double> situazionePrevista=new HashMap<LocalDate, Double>();
 		Map<LocalDate, Integer> situazioneCerta=new HashMap<LocalDate, Integer>();
 		int max=datidao.getNMaxPosti(reparto);
 		String result="";
+		post=false;
 		
-		for(Row r:simulazione) {
+		for(Row2 r:simulazione) {
 			situazionePrevista.put(r.getData(), (max-(r.getPrevisione()+r.getDomanda()))); //giorno e posti liberi secondo domanda+previsione
 		}
-		for(Row r:simulazione) {
+		for(Row2 r:simulazione) {
 			situazioneCerta.put(r.getData(), (max-(r.getDomanda()))); //giorno e posti liberi secondo domanda
 		}
 		
+		for(LocalDate data:situazionePrevista.keySet()) {
+			if(situazionePrevista.get(data)<=max) {
+				//siamo a posto
+			}
+			else {
+				//secondo la previsione non ci sono posti a sufficienza
+				if(situazioneCerta.get(data)<=max) {
+					//il posto ce ma devo valutare come muovermi se si avvera la mia previsione
+				}
+				else {
+					//non c'è sicuramente posto: cerco una soluzione.
+					//Se è un ricovero "spostabile" valuto altri reparti,
+					//valuto se è possibile posticiparlo se è programmato e valuto disponibilità giorni seguenti
+					//(apro opzione posticipa di tot giorni!)
+					post=true; //ripremere avvia simulazione
+					//se no rifiuto.
+				}
+			}
+		}
 		return result;
+	}
+	
+	public boolean posticipa() {
+		return post;
+	}
+
+	public Integer occMax(String reparto) {
+		return datidao.getNMaxPosti(reparto);
 	}
 
 }
